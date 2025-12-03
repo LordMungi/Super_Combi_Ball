a33c9a29c119a519ffefc47cb83fed1d843c0318
LordMungi <lrdmungi@gmail.com>
2025-12-03 19:48:31 -0300

    add force-based movement to player

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,9 +1,13 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 12.0
+const JUMP_STRENGTH = 8
+const AIR_RESISTANCE = 0.05
+
 const JUMP_VELOCITY = 4.5
 
+var hasJumped: bool
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -18,11 +22,17 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).rotated(Vector3.UP, $CameraArm.rotation.y)
-	if direction:
+	if direction and not hasJumped:
 		velocity.x = direction.x *  SPEED
 		velocity.z = direction.z * SPEED
+		velocity.y = JUMP_STRENGTH
+		hasJumped = true
+		$Timer.start()
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, AIR_RESISTANCE)
+		velocity.z = move_toward(velocity.z, 0, AIR_RESISTANCE)
+	
 	move_and_slide()
+
+func _on_timer_timeout() -> void:
+	hasJumped = false
