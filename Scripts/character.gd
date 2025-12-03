b0b5d9ad5e95ff879a8742ae8985f85ffae85d00
LordMungi <lrdmungi@gmail.com>
2025-12-03 20:12:29 -0300

    add move_up and move_down movements

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -14,9 +14,8 @@ func _physics_process(delta: float) -> void:
 	velocity.x = move_toward(velocity.x, 0, AIR_RESISTANCE * delta)
 	velocity.z = move_toward(velocity.z, 0, AIR_RESISTANCE * delta)
 		
-	if  not hasJumped:
+	if not hasJumped:
 		var direction
-		
 		if Input.is_action_just_pressed("move_forward"):
 			direction = Vector3(0, 0, -1).rotated(Vector3.UP, $CameraArm.rotation.y)
 			power = 600
@@ -33,6 +32,14 @@ func _physics_process(delta: float) -> void:
 			direction = Vector3(-1, 0, 0).rotated(Vector3.UP, $CameraArm.rotation.y)
 			power = 500
 			jumpStrength = 200
+		elif Input.is_action_just_pressed("move_up"):
+			direction = Vector3(0, 0, -1).rotated(Vector3.UP, $CameraArm.rotation.y)
+			power = 100
+			jumpStrength = 700
+		elif Input.is_action_just_pressed("move_down"):
+			direction = Vector3(0, 0, -1).rotated(Vector3.UP, $CameraArm.rotation.y)
+			power = 100
+			jumpStrength = -700
 		
 		if direction:
 			velocity.x = direction.x *  power * delta
