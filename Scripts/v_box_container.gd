803da8bf31bb5a2dd28d05b3fa6a5c8e332a72e0
LucasGalli <luquigalli0207@gmail.com>
2025-12-10 16:19:26 -0300

    created menu scene

---
+++ Scripts/v_box_container.gd
@@ -0,0 +1,27 @@
+extends VBoxContainer
+
+
+
+func _ready() -> void:
+	pass 
+
+
+
+func _process(delta: float) -> void:
+	
+	pass
+
+
+func _on_play_pressed() -> void:
+	
+	pass
+
+
+func _on_credits_pressed() -> void:
+	
+	pass 
+
+
+func _on_quit_pressed() -> void:
+	
+	pass
