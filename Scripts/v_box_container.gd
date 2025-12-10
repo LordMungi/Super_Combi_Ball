57da996656e4039d9333acd890064cc4f8568b80
LucasGalli <luquigalli0207@gmail.com>
2025-12-10 16:35:01 -0300

    set menu as main scene

--- Scripts/v_box_container.gd
+++ Scripts/v_box_container.gd
@@ -13,15 +13,19 @@ func _process(delta: float) -> void:
 
 
 func _on_play_pressed() -> void:
-	
+	get_tree().change_scene_to_file("res://Scenes/Game/game.tscn")
 	pass
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Settings/settings.tscn")
+	pass # Replace with function body.
 
 
 func _on_credits_pressed() -> void:
-	
+	get_tree().change_scene_to_file("res://Scenes/Credits/credits.tscn")
 	pass 
 
 
 func _on_quit_pressed() -> void:
-	
+	get_tree().quit()
 	pass
