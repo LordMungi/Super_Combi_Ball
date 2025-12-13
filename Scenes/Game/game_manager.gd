b63f5cf5785ebe38514fb3a56bb68e78d8398cba
Agustin <agustin.laure@estudiante.imagecampus.edu.ar>
2025-12-13 13:16:21 -0300

    add cheese asset as level objective

--- Scenes/Game/game_manager.gd
+++ Scenes/Game/game_manager.gd
@@ -2,20 +2,22 @@ extends Node3D
 
 signal game_won
 
-var player : CharacterBody3D
-var level : Node3D
 var spikes = []
 var flag = Area3D
+@onready var player : CharacterBody3D = %Character
+@onready var level : Node3D = %Level
+@onready var music : AudioStreamPlayer = %SuspenseMusic
 
 func _ready() -> void:
-	level = get_parent().get_node("Level")
 	spikes = level.get_tree().get_nodes_in_group("spikes")
-	player = get_parent().get_node("Character")
 	flag = level.get_node("Flag")
 	for spike in spikes:
 		spike.hitPlayer.connect(_on_player_hit)
 	pass 
 	flag.playerWon.connect(_on_player_won)
+	
+func _process(delta: float) -> void:
+		_loopAudio(music)
 
 func _on_player_hit() ->void:
 	player.global_position = player.checkPoint
@@ -23,4 +25,10 @@ func _on_player_hit() ->void:
 	
 func _on_player_won()->void:
 	emit_signal("game_won")
+	
+func _loopAudio(audio) -> void:
+	if audio:
+		if	!audio.playing:
+			audio.play()
 	pass
+	
--- Scripts/settings.gd
+++ Scripts/settings.gd
@@ -1,26 +1,20 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	pass
+	
 func _process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_ESCAPE):
 		get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
 	pass
 
-
 func _on_volume_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(0,value)
 	pass 
 
-
 func _on_mute_toggled(toggled_on: bool) -> void:
 	AudioServer.set_bus_mute(0,toggled_on)
-	pass # Replace with function body.
+	pass
 
 
 func _on_resolutions_item_selected(index: int) -> void:
@@ -31,12 +25,11 @@ func _on_resolutions_item_selected(index: int) -> void:
 			DisplayServer.window_set_size(Vector2i(1600,900))
 		2:
 			DisplayServer.window_set_size(Vector2i(1280,720))
-	pass # Replace with function body.
-
+	pass 
 
 func _on_check_box_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-	pass # Replace with function body.
+	pass 
